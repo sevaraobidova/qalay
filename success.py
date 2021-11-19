5c00cc060df2cd68601d11f198f6249d3d58b8ee
Sevara Obidova <94461050+sevaraobidova@users.noreply.github.com>
2021-11-19 22:36:22 +0500

    yana bir fayl qo'shildi
    biz hozir success degan faylni qo'shdik

---
+++ success.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  9 10:00:37 2021
+
+@author: Admin
+"""
+mevalar=['Olma', 'anor', 'nok']
+for meva in mevalar:
+    print(meva)
+print("Dastur tugadi")
